b835258b0b75297ea54e0fde873561457ed64754
Regasa <teshomerego@gmail.com>
2023-07-25 10:08:45 +0300

    Update 1-power.py

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,12 +1,8 @@
 def pow(a, b):
- a ^ b =1
-   # traversing in the range from 1 to given exponent+1
-   for i in range(1, b+1):
-      # Multiplying the result with the given number
-      a ^ b=a ^ b^a
-   # returning the resultant power
-   return a ^ b
-            # input number, exponent values
+if b == 0:
+    return 1
+else
+    return a*pow(a,b-1)
 print(pow(2, 2))
 print(pow(98, 2))
 print(pow(98, 0))
