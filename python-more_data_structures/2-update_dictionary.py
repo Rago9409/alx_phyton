ec9793d1f94a2f593cee2277dbf6874bb3d62c6b
Regasa <teshomerego@gmail.com>
2023-07-28 10:24:44 +0300

    Update 2-update_dictionary.py

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,3 +1,5 @@
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-
+def update_dictionary(a_dictionary, key:str, value):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary.update({key: value})
