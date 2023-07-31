c89e7e56decb617806c016dbcf8aa1be09ea465b
Regasa <teshomerego@gmail.com>
2023-07-31 14:33:32 +0300

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,17 +1,20 @@
 """This module defines a square with a private instance attribute called size.
-Args:
+    
+    Args:
         size (int): The size of the square.
     """
 class Square:
- """This class defines a square with a private instance attribute called size.
-
-Args:
+    """This class defines a square with a private instance attribute called size.
+    
+    Args:
         size (int): The size of the square.
     """
-def __init__(self, size):
+    def __init__(self, size):
         """
         The constructor for Square class.
+
         Parameters:
             size (int): The size of the square.
+
         """
         self.__size = size
