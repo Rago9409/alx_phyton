54d57d3c4491569bf5ff0471677be6314ba507cd
Regasa <teshomerego@gmail.com>
2023-07-31 14:18:53 +0300

    Update 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -24,3 +24,44 @@ class Square:
                 for j in range(self.__size):
                     print("#", end="")
                     print()
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    """
+    def __init__(self, size):
+        """
+        The constructor for Square class.
+
+        Parameters:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        This method calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        This method prints the square with the character #.
+
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+
