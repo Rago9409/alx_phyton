26d591f8997ac2d69a522511ce07bf3687d9f10c
Regasa <teshomerego@gmail.com>
2023-07-31 15:18:42 +0300

    Update 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -15,66 +15,66 @@ class Square:
     """
     def __init__(self, size=0):
         """
-        The constructor for Square class.
+    This function represents a square.
 
-        Parameters:
-            size (int): The size of the square.
+    Attributes:
+        __size (int): The size of the square.
 
-        """
+    """
         self.size = size
 
     @property
     def size(self):
-         """
+        """
     This function  represents a square setting property.
 
     Attributes:
         __size (int): The size of the square.
 
     """
-         return self.__size
-    
+        return self.__size
 
     @size.setter
     def size(self, value):
-         """
+        """
     This function represents a square with  setting property.
 
     Attributes:
         __size (int): The size of the square.
 
     """
-         if not isinstance(value, int):
-                 """  This Checks  a square value is integer.
+        if not isinstance(value, int):
+            """
+    This Checks  a square value is integer.
 
     Attributes:
         __size (int): The size of the square.
 
     """
-         raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         elif value < 0:
-    """
+            """
     This checks  a square value not integer.
 
     Attributes:
         __size (int): The size of the square.
 
     """
-    raise ValueError("size must be >= 0")
-    self.__size = value
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """
-        This method calculates the area of the square.
+    This function returns the current square area.
 
-        Returns:
-            int: The area of the square.
+    Attributes:
+        __size (int): The size of the square.
 
-        """
+    """
         return self.__size ** 2
-    def my_print(self):
+def my_print(self):
         """
-        This method prints the square with the character #.
+        This method prints empty line or the square with the character #.
 
         """
         if self.__size == 0:
@@ -84,7 +84,10 @@ class Square:
         """
             print()
         else:
-            for i in range(self.__size):
+              """
+        This function prints the square with the character #.
+        """
+        for i in range(self.__size):
                 for j in range(self.__size):
                     print("#", end="")
                     print()
