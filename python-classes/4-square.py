9b7a218cd4a9dfd786535577f61cf3b2de2ea499
Regasa <teshomerego@gmail.com>
2023-07-31 15:11:14 +0300

    Update 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,29 +1,10 @@
-class Square:
-    def __init__(self, size=0):
-        self.size = size
+"""
+    This program code  represents a square.
 
-    @property
-    def size(self):
-        return self.__size
+    Attributes:
+        __size (int): The size of the square.
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        return self.__size ** 2
-    def my_print(self):
-        if self.__size == 0:
-            print()
-        else:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end="")
-                    print()
+    """
 class Square:
     """
     This class represents a square.
@@ -32,7 +13,7 @@ class Square:
         __size (int): The size of the square.
 
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
         The constructor for Square class.
 
@@ -40,7 +21,47 @@ class Square:
             size (int): The size of the square.
 
         """
-        self.__size = size
+        self.size = size
+
+    @property
+    def size(self):
+         """
+    This function  represents a square setting property.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    """
+         return self.__size
+    
+
+    @size.setter
+    def size(self, value):
+         """
+    This function represents a square with  setting property.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    """
+         if not isinstance(value, int):
+                 """  This Checks  a square value is integer.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    """
+         raise TypeError("size must be an integer")
+        elif value < 0:
+    """
+    This checks  a square value not integer.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    """
+    raise ValueError("size must be >= 0")
+    self.__size = value
 
     def area(self):
         """
@@ -51,17 +72,19 @@ class Square:
 
         """
         return self.__size ** 2
-
     def my_print(self):
         """
         This method prints the square with the character #.
 
         """
         if self.__size == 0:
+            """
+        This function prints the empty line.
+
+        """
             print()
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
                     print("#", end="")
-                print()
-
+                    print()
