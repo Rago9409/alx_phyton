3ea1ff8a078e56ea3b02fd293ec97c09732d3998
Regasa <teshomerego@gmail.com>
2023-08-04 11:42:58 +0300

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -3,9 +3,9 @@ class BaseGeometry:
     """This Function raises an Exception with the message area() is not implemented"""
     def area(self):
         raise Exception("area() is not implemented")
+    """This Function validates value"""
     def integer_validator(self, name, value):
-        """This Function that validates a value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
