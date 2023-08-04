9e31168b02c00c491b9ed58ee9d18162828d817d
Regasa <teshomerego@gmail.com>
2023-08-04 10:43:58 +0300

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,4 +1,6 @@
+"""This is a class Rectangle"""
 class Rectangle:
+    """This Function instantiates width and height """
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
