f75d4d07731f95cf12129a6bfd5ae21d20208391
Regasa <teshomerego@gmail.com>
2023-08-04 14:05:14 +0300

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,4 +1,5 @@
 """This is a class BaseGeometry"""
+class BaseGeometry: 
     """This Function raises an Exception with the message area() is not implemented"""
     def area(self):
         raise Exception("area() is not implemented")
