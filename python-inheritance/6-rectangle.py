5a8af72f2df0d76ed571419bc01d45b263983a1f
Regasa <teshomerego@gmail.com>
2023-08-04 14:12:04 +0300

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,12 +1,12 @@
-"""This is a class BaseGeometry"""
-class BaseGeometry: 
+"""This is class BaseGeometry."""
+class BaseGeometry:
     """This Function raises an Exception with the message area() is not implemented"""
     def area(self):
         raise Exception("area() is not implemented")
     """This Function validates value"""
     def integer_validator(self, name, value):
         if type(value) != int:
-            raise TypeError(f"{name} must be an integer")f
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 """This is a class Rectangle inherits class BaseGeometry"""
