74361c6da9d3798600ff2b756885ebba202073da
Regasa <teshomerego@gmail.com>
2023-08-04 10:43:50 +0300

    Update 4-base_geometry.py

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,3 +1,5 @@
+"""This is class BaseGeometry."""
 class BaseGeometry:
+    """This Function raises an Exception with the message area() is not implemented"""
     def area(self):
         raise Exception("area() is not implemented")
