385b5b922a99c678d6df5cde90ae3fe1fe41268f
Regasa <teshomerego@gmail.com>
2023-08-04 10:42:24 +0300

    Update 0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,6 +1,4 @@
-"""This code program  defines a square with a size
-    Args:
-        size (int): The size of the square.
+"""This code program  defines a class is the same.
     """
 def is_same_class(obj, a_class):
     """This code program  prints object of same class.
