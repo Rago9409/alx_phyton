46be37b08f9c4fc96e4a6d25631c5ea6f166c098
Regasa <teshomerego@gmail.com>
2023-08-04 10:43:24 +0300

    Update 7-rectangle.py

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,12 +1,14 @@
+"""This is a class Rectangle"""
 class Rectangle:
+    """This Function instantiates width and height """
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
+    """This method returns  width and height """
     def area(self):
         return self.__width * self.__height
-
+    """This Function should return str """
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
