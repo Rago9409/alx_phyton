9a8c8749965bdda3e17d633623f0fd78942d3f93
Regasa <teshomerego@gmail.com>
2023-08-04 10:43:14 +0300

    Update 8-square.py

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,7 +1,9 @@
+"""This is a class Square"""
 class Square:
+    """This Function instantiates size """
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
-
+    """This should return str """
     def __str__(self):
         return "[Square] {}/{}".format(self.__width, self.__height)
