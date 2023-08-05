44a861198a8b508fefa52837c7d0afd8418dd3ab
Regasa <teshomerego@gmail.com>
2023-08-05 10:21:34 +0300

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,10 +1,9 @@
-"""This code program  defines a class Base that is the base 
-class aimed to manage id attribute.
-    """
+"""This is module for Base class."""
 class Base:
+ """This code program  defines a class Base that is 
+ the base class aimed to manage id attribute."""
  __nb_objects = 0
- """This code program defines a class constructor.
-    """
+ """This code program defines a class constructor."""
 def __init__(self, id=None):
         if id is not None:
             self.id = id
