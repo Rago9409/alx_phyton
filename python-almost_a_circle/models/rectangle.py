7b2786a6751b51be58813baaba7f70f1a8e74ebf
Regasa <teshomerego@gmail.com>
2023-08-05 11:14:03 +0300

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,30 +1,7 @@
 """
 Module for Rectangle class
 """
-class Base:
-    """
-    Class for base objects
-    """
-
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """
-        Initializes a Base object
-
-        Args:
-            id (int): unique identifier for base object
-
-        Returns:
-            None
-        """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
-
+from models.base import Base
 class Rectangle(Base):
     """
     Class for rectangle objects
