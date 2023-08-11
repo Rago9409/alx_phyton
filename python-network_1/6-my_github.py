6d1a8e91d0701bd7c36c771bfc49fae463e433bb
Regasa <teshomerego@gmail.com>
2023-08-11 16:07:49 +0300

    Update 6-my_github.py

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,8 +1,12 @@
-"""Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id"""
+"""Python script takes your GitHub credentials (username and password) and uses the GitHub API to display your id"""
 import requests
 import sys
+
 username = sys.argv[1]
 password = sys.argv[2]
+
 url = "https://api.github.com/user"
+
 response = requests.get(url, auth=(username, password))
+
 print(response.json()["id"])
