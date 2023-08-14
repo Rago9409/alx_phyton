c90203aab3e9e33cad130860b9e79d76676a665e
Regasa <teshomerego@gmail.com>
2023-08-14 10:36:28 +0300

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,11 +1,24 @@
-"""This is class BaseGeometry."""
+"""Defines a base geometry class BaseGeometry."""
+
+
 class BaseGeometry:
-    """This Function raises an Exception with the message area() is not implemented"""
+    """Reprsent base geometry."""
+
     def area(self):
+        """Not yet implemented."""
         raise Exception("area() is not implemented")
-    """This Function validates value"""
+
     def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
         if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
