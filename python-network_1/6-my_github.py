30eaad1f63b61abde430066a45902065db28c328
Regasa <teshomerego@gmail.com>
2023-08-14 10:50:18 +0300

    Update 6-my_github.py

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,6 +1,6 @@
 """Uses the GitHub API to display a GitHub ID based on given credentials.
 
-Usage: ./10-my_github.py <GitHub username> <GitHub password>
+Usage: ./6-my_github.py <GitHub username> <GitHub password>
   - Uses Basic Authentication to access the ID.
 """
 import sys
