4a086aa809db2b1f819cdde7b8f0f74b7af4f0dd
Regasa <teshomerego@gmail.com>
2023-08-15 14:24:41 +0300

    Update 7-rectangle.py

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,27 +1,28 @@
-"""This is a class BaseGeometry"""
-class BaseGeometry:
-    """This Function raises an Exception with the message area() is not implemented"""
-    def area(self):
-        raise Exception("area() is not implemented")
-    """This Function validates value"""
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-"""This is a class Rectangle inherits class BaseGeometry"""
-class Rectangle(BaseGeometry):
-    """This Function instantiates width and height """
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-    """This method returns the area of the rectangle. """     
-    def area(self):
-        return self.__width * self.__height
-    """This method returns string representation of the rectangle. """
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-    
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
