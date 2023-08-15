8f617090d556b9b40ba8d32b84e5e7392a5cdeb2
Regasa <teshomerego@gmail.com>
2023-08-15 14:05:31 +0300

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,38 +1,21 @@
-"""This module defines a square with a private instance attribute called size.
+"""Define a class Square."""
 
-    Args:
-        size (int): The size of the square.
-    """
+
 class Square:
-    """This Class defines a square with a private instance attribute called size.
-    
-    Args:
-        size (int): The size of the square.
-    """
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """
-        The constructor for Square class.
+        """Initialize a new square.
 
-        Parameters:
-            size (int): The size of the square.
-
+        Args:
+            size (int): The size of the new square.
         """
-        self.__size = size
-
-        if type(size) != int:
-            """
-        The size must integer.
-        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            """
-         The size of the square less than 0.
-
-        """
             raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        """This function defines area of  square with instance attribute called size.
-    
-    """
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
