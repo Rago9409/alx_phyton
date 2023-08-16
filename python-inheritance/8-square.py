a245da5cf08e60b32628979dd6a6b93228dc688d
Regasa <teshomerego@gmail.com>
2023-08-16 10:48:18 +0300

    Update 8-square.py

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -15,7 +15,6 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-        square=Square(6)
         return dir((Square))
 
      
