2bf8eca005b572986cc78a33808b8fba1bd79923
Regasa <teshomerego@gmail.com>
2023-08-22 10:59:07 +0300

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,3 +1,7 @@
+# Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
+# Usage: ./4-cities_by_state.py <mysql username> \
+#                               <mysql password> \
+#                               <database name>
 import sys
 import MySQLdb
 
