b7e3d638d2a02805875d0d91a5aa62325e6d436d
Regasa <teshomerego@gmail.com>
2023-08-26 11:28:31 +0300

    Update 0-hello_route.py

---
+++ python-web_framework/0-hello_route.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!' when the root URL is navigated to.
+
+    Returns:
+        str: The message 'Hello HBNB!'
+    """
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
+    
