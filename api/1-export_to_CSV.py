f6db6f4f1098bc42da7af36bd0ebed51656b8745
Regasa <teshomerego@gmail.com>
2023-09-30 09:14:45 +0300

    Create 1-export_to_CSV.py

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+import csv
+import requests
+
+def export_employee_todo_to_csv(employee_id):
+    # Endpoint URLs
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+
+    # Fetching employee details
+    employee_response = requests.get(employee_url)
+    employee_data = employee_response.json()
+    employee_name = employee_data["name"]
+
+    # Fetching TODO list
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    # Creating CSV file
+    csv_filename = f"{employee_id}.csv"
+    with open(csv_filename, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for task in todos_data:
+            writer.writerow([employee_id, employee_name, task["completed"], task["title"]])
+
+    print(f"CSV file '{csv_filename}' has been created successfully.")
+
+
