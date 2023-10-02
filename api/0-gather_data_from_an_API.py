554693ab93d8c2aa71dedd36ef8addfcc1ede48d
Regasa <teshomerego@gmail.com>
2023-10-02 09:28:28 +0300

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,33 +1,27 @@
-import json
 import requests
+import sys
 
-def export_all_employee_todo_to_json():
-    # Endpoint URL
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
-    employee_url = "https://jsonplaceholder.typicode.com/users"
+def get_employee_todo_progress(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url)
+    employee = response.json()
 
-    # Fetching employee details
-    employee_response = requests.get(employee_url)
-    employee_data = employee_response.json()
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(url)
+    todos = response.json()
 
-    # Creating JSON object
-    json_data = {}
-    for employee in employee_data:
-        employee_id = str(employee["id"])
-        employee_name = employee["name"]
-        json_data[employee_id] = []
-        todos_response = requests.get(f"{todos_url}?userId={employee_id}")
-        todos_data = todos_response.json()
-        for task in todos_data:
-            json_data[employee_id].append({
-                "task": task["title"],
-                "completed": task["completed"],
-                "username": employee_name
-            })
+    total_tasks = len(todos)
+    done_tasks = sum(1 for todo in todos if todo['completed'])
 
-    # Creating JSON file
-    json_filename = "todo_all_employees.json"
-    with open(json_filename, "w") as jsonfile:
-        json.dump(json_data, jsonfile)
+    print(f"Employee {employee['name']} is done with tasks({done_tasks}/{total_tasks}):")
+    for todo in todos:
+        if todo['completed']:
+            print(f"\t {todo['title']}")
 
-    print(f"JSON file '{json_filename}' has been created successfully.")
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} EMPLOYEE_ID")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    get_employee_todo_progress(employee_id)
