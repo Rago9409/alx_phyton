b0b0ca30fb643b614bd7cb81867962770dfee20f
Regasa <teshomerego@gmail.com>
2023-10-03 11:02:15 +0300

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,36 +3,29 @@
 
 import csv
 import requests
-import sys
 
+def get_employee_todo_list_progress(employee_id):
+    # Get employee details
+    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    employee_name = response.json()['name']
 
-def get_employee_todo_progress(employee_id):
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    response = requests.get(url)
-    employee = response.json()
-
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    response = requests.get(url)
+    # Get employee TODO list
+    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
     todos = response.json()
-    
-    total_tasks = len(todos)
-    done_tasks = sum(1 for todo in todos if todo['completed'])
 
-    print(f"Employee {employee['name']} is done with tasks({done_tasks}/{total_tasks}):")
+    # Calculate progress
+    total_tasks = len(todos)
+    done_tasks = len([todo for todo in todos if todo['completed']])
+
+    # Print progress report
+    print(f'Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):')
     for todo in todos:
         if todo['completed']:
-            print(f"\t {todo['title']}")
+            print(f'\t{todo["title"]}')
 
-    with open(f"{employee_id}.csv", mode='w', newline='') as csvfile:
+    # Export data to CSV file
+    with open(f'{employee_id}.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
         for todo in todos:
-            writer.writerow([employee_id, employee['name'], todo['completed'], todo['title']])
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} EMPLOYEE_ID")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    get_employee_todo_progress(employee_id)
+            writer.writerow([employee_id, employee_name, todo['completed'], todo['title']])
