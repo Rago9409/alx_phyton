8c24a772f4f527c4ab34e1b03433186126d2810f
Regasa <teshomerego@gmail.com>
2023-10-09 10:09:24 +0300

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,18 +1,40 @@
-#!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""fetching employee,TODO lists and counting completed tasks
+"""
+
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+def get_employee_todo_list_progress(employee_id):
+    # Get employee details
+    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    employee_name = response.json()['name']
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+    # Get employee TODO list
+    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
+    todos = response.json()
+
+    # Calculate progress
+    total_tasks = len(todos)
+    done_tasks = len([todo for todo in todos if todo['completed']])
+
+    # Print progress report
+    print(f'Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):')
+    for todo in todos:
+        if todo['completed']:
+            print(f'\t{todo["title"]}')
+
+    # Export data to CSV file
+    with open(f'{employee_id}.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+        for todo in todos:
+            writer.writerow([employee_id, employee_name, todo['completed'], todo['title']])
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} EMPLOYEE_ID")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    get_employee_todo_list_progress(employee_id)
